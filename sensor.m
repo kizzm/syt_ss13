e51853c6b5945cd11b5a5b38ab608fddcf5f980c
Sebastian <s.schleich@tri-b-oon.de>
2013-06-13 14:08:36 +0200

    Start implementierung sensor

---
+++ sensor.m
@@ -0,0 +1,35 @@
+%% signal = sensor( Blendenwinkel, Unit, Sensorkonstanten)
+% Erzeugt:
+%       [Out]:
+%       Spannungssignal => signalV in V
+%       
+%       [In]:
+%       aus Eingangswinkel => Blendenwinkel in 
+%       der angegebenen Einheit => Unit
+%       Array aus Sensorkonstanten => Sensorkonstanten
+%           1 signal_max in A
+%           2 Lastwiederstand der Idealen Photodiode in Ohm
+%       Unit cases: 'grad','mgrad','rad','mrad'
+
+function signal = sensor(Blendenwinkel,Unit,Sensorkonstante)
+
+%% Abfrage der Eingangseinheit
+switch Unit
+    case 'grad'
+        phiB = Blendenwinkel/180*pi        
+    case 'mgrad'
+        phiB = Blendenwinkel/1000/180*pi
+    case 'rad'
+        phiB = Blendenwinkel
+    case 'mrad'
+        phiB = Blendenwinkel/1000
+        
+a = @(phi) 1/20/180*pi * phi + 0,5 % lineares Verhalten des Sensors +- 10°
+
+S13 = @(phi) Sensorkonstanten(1)*(1-a(phi)) % Signal der Sensoren 1+3
+S24 = @(phi) Sensorkonstanten(1)*a(phi) % Signal der Sensoren 2+4
+
+Iph = @(phi) S13(phi) - S24(phi) % Gesammt Photostrom
+
+signal = Sensorkonstanten(2)*Iph(phiB) % Spannungssignal [Out]
+end
---
+++ sensorDaten.m
@@ -0,0 +1,19 @@
+%% Sensorkonstanten = sensorDaten(Photodioden,Blendenbreite,LED)
+% Erzeugt:
+%       [Out]:
+%       Array der Sensorkonstanten => Sensorkonstanten
+%       
+%       [In]:
+%       Array der Diodendaten => Photodioden
+%           1   Azimutwinkel                in rad
+%           2   Öffnungswinkel              in rad      
+%           3   Innererradius r1            in mm
+%           4   Äußererradius r2            in mm
+%       Breite der Blende => Blendenbreite  in rad
+%       Array der LEDDaten => LED
+%           1   LED Abstrahlcharakteristik  als Array
+%           2   LED Abstand zu Dioden       in mm
+
+function Sensorkonstanten = sensorDaten(Photodioden,Blendenbreite,LED)
+
+end
