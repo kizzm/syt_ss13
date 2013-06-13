0cdb5aca10c8cb67706f4e8a9651b36bf9c3bbdc
Sebastian <s.schleich@tri-b-oon.de>
2013-06-13 14:20:52 +0200

    änderungen in sensor.m

--- sensor.m
+++ sensor.m
@@ -11,25 +11,26 @@
 %           2 Lastwiederstand der Idealen Photodiode in Ohm
 %       Unit cases: 'grad','mgrad','rad','mrad'
 
-function signal = sensor(Blendenwinkel,Unit,Sensorkonstante)
+function signal = sensor(Blendenwinkel,Unit,Sensorkonstanten)
 
 %% Abfrage der Eingangseinheit
 switch Unit
     case 'grad'
-        phiB = Blendenwinkel/180*pi        
+        phiB = Blendenwinkel/180*pi;    
     case 'mgrad'
-        phiB = Blendenwinkel/1000/180*pi
+        phiB = Blendenwinkel/1000/180*pi;
     case 'rad'
-        phiB = Blendenwinkel
+        phiB = Blendenwinkel;
     case 'mrad'
-        phiB = Blendenwinkel/1000
+        phiB = Blendenwinkel/1000;
+end
         
-a = @(phi) 1/20/180*pi * phi + 0,5 % lineares Verhalten des Sensors +- 10°
+a = @(phi) 1/(20/180*pi) * phi + 0.5; % lineares Verhalten des Sensors +- 10°
 
-S13 = @(phi) Sensorkonstanten(1)*(1-a(phi)) % Signal der Sensoren 1+3
-S24 = @(phi) Sensorkonstanten(1)*a(phi) % Signal der Sensoren 2+4
+S13 = @(phi) Sensorkonstanten(1)*(a(phi)); % Signal der Sensoren 1+3
+S24 = @(phi) Sensorkonstanten(1)*(1-a(phi)); % Signal der Sensoren 2+4
 
-Iph = @(phi) S13(phi) - S24(phi) % Gesammt Photostrom
+Iph = @(phi) S13(phi) - S24(phi); % Gesammt Photostrom
 
-signal = Sensorkonstanten(2)*Iph(phiB) % Spannungssignal [Out]
+signal = Sensorkonstanten(2)*Iph(phiB); % Spannungssignal [Out]
 end
